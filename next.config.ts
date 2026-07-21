983bd559ef810ac6b7e4d925691ecd37df9da211
silpawar <silpawar@deloitte.com.au>
2026-07-21 16:00:49 +1000

    feat: config updated to produce the logs

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,17 @@ const nextConfig: NextConfig = {
   },
   basePath,
   assetPrefix: basePath,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+    // Options: 'warn' (recommended) or true (for all logs)
+    browserToTerminal: true,
+  },
+  compiler: {
+    // REMOVE or comment out this block to allow console logs in production
+    removeConsole: false,
+  },
 };
 
 export default nextConfig;
