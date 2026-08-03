ee497c6b26b902ac9abb18b83ecdb9cf8f819b94
steph150 <ssar@deloitte.com.au>
2026-08-03 16:04:30 +1000

    Update query values

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import { queryAEM } from './lib/aem-client';
-import CaravanFormClient from '@/app/CaravanFormClient';
-import type { CaravanContentResponseData } from '@/app/types/ContentTypes';
-import './page.css';
+import { queryAEM } from "./lib/aem-client";
+import CaravanFormClient from "@/app/CaravanFormClient";
+import type { CaravanContentResponseData } from "@/app/types/ContentTypes";
+import "./page.css";
 
 export default async function Home() {
   let caravanData: CaravanContentResponseData | null = null;
 
   try {
     caravanData = await queryAEM<CaravanContentResponseData>(
-      'caravanContentByPath',
-      { path: '/content/dam/wknd-shared/caravan-content' }
+      "insurance-journey-content",
+      { path: "/content/dam/wknd-shared/caravan/caravan-insurance-journey" },
     );
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
