085b835f2066c36bcc02d5863ee9b3944827d4d5
steph150 <ssar@deloitte.com.au>
2026-08-20 10:07:49 +1000

    Revert change - add back step instrumentation

--- app/CaravanFormClient.tsx
+++ app/CaravanFormClient.tsx
@@ -770,11 +770,11 @@ export default function CaravanFormClient({
                   className="caravan-author-step-section"
                   style={authorScrollTargetStyle}
                   data-step={step.id}
-                  // data-aue-resource={insuranceJourneyStepResource}
+                  data-aue-resource={insuranceJourneyStepResource}
                   // data-aue-type="component"
-                  // data-aue-type="container"
-                  // data-aue-label={`Step ${step.id}`}
-                  // data-aue-model={INSURANCE_JOURNEY_STEP_MODEL_IDS[step.id]}
+                  data-aue-type="container"
+                  data-aue-label={`Step ${step.id}`}
+                  data-aue-model={INSURANCE_JOURNEY_STEP_MODEL_IDS[step.id]}
                   open={expandedAuthorStepId === step.id}
                 >
                   <summary
