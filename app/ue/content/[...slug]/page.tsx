4146024ba947778adb1f533f889ab82ea4b4162b
steph150 <ssar@deloitte.com.au>
2026-08-25 18:26:12 +1000

    Revert UE preview changes

--- app/ue/content/[...slug]/page.tsx
+++ app/ue/content/[...slug]/page.tsx
@@ -1,26 +1,26 @@
-import { notFound } from "next/navigation";
-import PageContent from "@/app/PageContent";
-import { resolveContentRoute } from "@/app/lib/contentRoute";
+// import { notFound } from "next/navigation";
+// import PageContent from "@/app/PageContent";
+// import { resolveContentRoute } from "@/app/lib/contentRoute";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
-export default async function UniversalEditorContentPage({
-  params,
-}: {
-  params: Promise<{ slug: string[] }>;
-}) {
-  const { slug } = await params;
-  const contentRoute = resolveContentRoute(slug);
+// export default async function UniversalEditorContentPage({
+//   params,
+// }: {
+//   params: Promise<{ slug: string[] }>;
+// }) {
+//   const { slug } = await params;
+//   const contentRoute = resolveContentRoute(slug);
 
-  if (!contentRoute) {
-    notFound();
-  }
+//   if (!contentRoute) {
+//     notFound();
+//   }
 
-  return (
-    <PageContent
-      config={contentRoute.config}
-      authorStep={contentRoute.authorStep}
-      aemTarget="preview"
-    />
-  );
-}
+//   return (
+//     <PageContent
+//       config={contentRoute.config}
+//       authorStep={contentRoute.authorStep}
+//       aemTarget="preview"
+//     />
+//   );
+// }
